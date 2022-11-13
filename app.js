ba626bf6a15ecb96155af049b4fca3548a04eb30
Larissa Jasso <larissajasso@live.com.mx>
2022-11-13 03:09:22 -0700

    Funcionalidad para agregar al carrito

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 const productCard = document.getElementById("product-card").content;
 const products = document.getElementById("products");
+const carProducts = document.getElementById("car-products");
+const footer = document.getElementById("footer");
+const orden = document.getElementById("orden").content;
+const templateCarPrd = document.getElementById("template-car-products").content;
 const categories = document.getElementById("category");
-const hcList = document.getElementById("hclist");
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 const sorterAlf = document.getElementById("filter-alfabetico");
@@ -14,8 +17,9 @@ const sorterPrice = document.getElementById("filter-precio");
 const sorterDesc = document.getElementById("filter-descuento");
 const fragment = document.createDocumentFragment();
 let data = {};
+let car_products = {};
 var current_page = 1;
-var records_per_page = 10;
+var records_per_page = 5;
 var num_pages = 0;
 let dropdown = document.getElementById("category");
 dropdown.length = 0;
@@ -37,7 +41,6 @@ searchInput.addEventListener("keypress", function (event) {
   }
 });
 searchInput.addEventListener("input", (e) => {
-  console.log(searchInput.value);
   if (searchInput.value == "") {
     fetchData();
   }
@@ -67,6 +70,14 @@ sorterDesc.addEventListener("change", function () {
   }
 });
 
+products.addEventListener("click", (e) => {
+  addProduct(e);
+});
+
+carProducts.addEventListener("click", (e) => {
+  modificarProducts(e);
+});
+
 const fetchData = async () => {
   const product = await fetch("http://127.0.0.1:8000/api/products");
   const res = await product.json();
@@ -82,7 +93,7 @@ const fetchCategory = async () => {
 const listProducts = (product) => {
   product.forEach((item) => {
     productCard.querySelector("h5").textContent = item.name;
-    productCard.querySelector("p").textContent = "Precio $" + item.price;
+    productCard.querySelector("p").textContent = item.price;
     productCard.querySelector("label").textContent =
       "Descuento $" + item.discount;
     const image = productCard.querySelector("img");
@@ -210,3 +221,108 @@ function numPages() {
   num_pages = Math.ceil(Object.values(data).length / records_per_page);
   return num_pages;
 }
+
+const addProduct = (e) => {
+  if (e.target.classList.contains("btn-warning")) {
+    prod = e.target.parentElement;
+    const producto = {
+      title: prod.querySelector("h5").textContent,
+      precio: prod.querySelector("p").textContent,
+      id: prod.querySelector("button").dataset.id,
+      cantidad: 1,
+    };
+    // console.log(producto)
+    if (car_products.hasOwnProperty(producto.id)) {
+      producto.cantidad = car_products[producto.id].cantidad + 1;
+    }
+
+    car_products[producto.id] = { ...producto };
+
+    showProducts();
+  }
+  e.stopPropagation();
+};
+
+const showProducts = () => {
+  carProducts.innerHTML = "";
+
+  Object.values(car_products).forEach((producto) => {
+    templateCarPrd.querySelector("th").textContent = producto.id;
+    templateCarPrd.querySelectorAll("td")[0].textContent = producto.title;
+    templateCarPrd.querySelectorAll("td")[1].textContent = producto.cantidad;
+    templateCarPrd.querySelectorAll("td")[2].textContent =
+      producto.precio * producto.cantidad;
+
+    //botones
+    templateCarPrd.querySelector(".btn-success").dataset.id = producto.id;
+    templateCarPrd.querySelector(".btn-danger").dataset.id = producto.id;
+    templateCarPrd.querySelector(".btn-outline-danger").dataset.id =
+      producto.id;
+
+    const clone = templateCarPrd.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  carProducts.appendChild(fragment);
+
+  showOrder();
+};
+
+const showOrder = () => {
+  footer.innerHTML = "";
+  if (Object.keys(car_products).length === 0) {
+    footer.innerHTML = `
+        <label>No hay productos añadidos</label>
+        `;
+    return;
+  }
+  const nCantidad = Object.values(car_products).reduce(
+    (acc, { cantidad }) => acc + cantidad,
+    0
+  );
+  const nPrecio = Object.values(car_products).reduce(
+    (acc, { cantidad, precio }) => acc + cantidad * precio,
+    0
+  );
+
+  orden.querySelectorAll("label")[1].textContent = nCantidad;
+  orden.querySelectorAll("label")[3].textContent = "$" + nPrecio;
+
+  const clone = orden.cloneNode(true);
+  fragment.appendChild(clone);
+
+  footer.appendChild(fragment);
+
+  const boton = document.querySelector('#pagar')
+    boton.addEventListener('click', () => {
+        carrito = {}
+        showProducts()
+    })
+};
+
+const modificarProducts = (e) => {
+  if (e.target.classList.contains("btn-success")) {
+    const producto = car_products[e.target.dataset.id];
+    producto.cantidad++;
+    car_products[e.target.dataset.id] = { ...producto };
+    showProducts();
+  }
+
+  if (e.target.classList.contains("btn-danger")) {
+    const producto = car_products[e.target.dataset.id];
+    producto.cantidad--;
+    if (producto.cantidad === 0) {
+      delete car_products[e.target.dataset.id];
+    } else {
+      car_products[e.target.dataset.id] = { ...producto };
+    }
+    showProducts();
+  }
+
+  if (e.target.classList.contains("btn-outline-danger")) {
+    delete car_products[e.target.dataset.id];
+
+    showProducts();
+  }
+  e.stopPropagation();
+};
+
